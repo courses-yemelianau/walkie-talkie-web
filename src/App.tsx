b3617c7aa2afadf229fa8f0ecdb3d79ad0b78443
Siarhei Yemelianau <xemelyanov@gmail.com>
2023-06-11 03:07:52 +0300

    🐛 fix(App.tsx): change setUsername to setUser to reflect the change in the appSlice

--- src/App.tsx
+++ src/App.tsx
@@ -3,21 +3,22 @@ import { Container } from 'react-bootstrap';
 import SendMessageForm from './components/SendMessageForm';
 import ReceivedMessages from './components/ReceivedMessages';
 import UserForm from './components/UserForm';
-import { setUsername } from './redux/appSlice';
+import { setUser } from './redux/appSlice';
 import { useAppDispatch, useAppSelector } from './hooks';
+import { User } from './interfaces';
 
 const App: React.FC = () => {
     const dispatch = useAppDispatch();
-    const { username } = useAppSelector(state => state.app);
+    const { user } = useAppSelector(state => state.app);
 
-    const handleStartChat = (newUserName: string) => {
-        dispatch(setUsername(newUserName));
+    const handleStartChat = (newUser: User) => {
+        dispatch(setUser(newUser));
     };
 
     return (
         <Container className="mt-4">
-            <UserForm name={username} onSubmit={handleStartChat} />
-            {username && (
+            <UserForm name={user.name} onSubmit={handleStartChat} />
+            {user.id && (
                 <>
                     <SendMessageForm />
                     <hr />
